65bbd63b9c9927c1ab5e2ac0aa3253b93806c9d8
Danny Andrews <lunde.andrews@gmail.com>
2021-01-03 15:00:29 -0600

    Add horizontal LabeledInput variation

--- src/components/Atoms/LabeledInput/component.jsx
+++ src/components/Atoms/LabeledInput/component.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import styles from "./styles.module.css";
 import cn from "classnames";
 
-const LabeledInput = ({ label, children, className }) =>
+const LabeledInput = ({ label, children, className, horizontal = false }) =>
   label ? (
-    <label className={cn(styles.root, className)}>
+    <label
+      className={cn(styles.root, className, {
+        [styles.horizontal]: horizontal,
+        [styles.vertical]: !horizontal,
+      })}
+    >
       <div className={styles["label-text"]}>{label}</div>
       {children}
     </label>
